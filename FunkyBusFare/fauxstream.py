87635771776815b0597b22599059a893d6ce00bc
Doug Shawhan <doug.shawhan@gmail.com>
2021-02-27 11:06:09 -0500

    cleaned up inheritance

--- FunkyBusFare/fauxstream.py
+++ FunkyBusFare/fauxstream.py
@@ -1,24 +1,28 @@
-import json
-import gzip
 """
 Write lists of json objects to a file without a lot of fuss.
 use GZ version for compression
 """
 
-class JSONList(object):
+import json
+import gzip
+
+
+class JSONList:
     """
     "Stream" json-serializable objects to a file as a json list-of-objects.
     """
+
     openbracket = "[\n"
     closebracket = "\n]"
+
     def __init__(self, path):
         """
         Create a file handle.
         Create a blank "comma" object
         Accepts path(str) output path
         """
-        self.fh = open(path, "w")
-        self.comma=comma()
+        self.filehandle = open(path, "w")
+        self.comma = comma()
         self.bytecount = 0
 
     def __enter__(self):
@@ -26,7 +30,7 @@ class JSONList(object):
         Upon entry signal, write our opening bracket and a newline.
         return self
         """
-        self.fh.write(self.openbracket)
+        self.filehandle.write(self.openbracket)
         return self
 
     def __exit__(self, _type, value, traceback):
@@ -34,8 +38,8 @@ class JSONList(object):
         Upon exit singnal, write a newline and our closing bracket, then
         close file handle
         """
-        self.fh.write(self.closebracket)
-        self.fh.close()
+        self.filehandle.write(self.closebracket)
+        self.filehandle.close()
 
     def write(self, obj):
         """
@@ -45,7 +49,8 @@ class JSONList(object):
         """
         val = "".join([next(self.comma), json.dumps(obj)])
         self.bytecount += len(val)
-        self.fh.write(val)
+        self.filehandle.write(val)
+
 
 class JSONListGZ(JSONList):
     """
@@ -62,8 +67,9 @@ class JSONListGZ(JSONList):
         Create a blank "comma" object
         Accepts path(str) output path
         """
-        self.fh = gzip.open(path, "w")
-        self.comma=comma()
+        super().__init__(path)
+        self.filehandle = gzip.open(path, "w")
+        self.comma = comma()
         self.bytecount = 0
 
     def write(self, obj):
@@ -74,7 +80,8 @@ class JSONListGZ(JSONList):
         """
         val = "".join([next(self.comma), json.dumps(obj)]).encode()
         self.bytecount += len(val)
-        self.fh.write(val)
+        self.filehandle.write(val)
+
 
 def comma():
     """
@@ -87,6 +94,7 @@ def comma():
     yield ""
     while True:
         yield ",\n"
+
 
 def test():
     """
@@ -102,38 +110,31 @@ def test():
     >>> from FunkyBusFare.fauxstream import test
     >>> TMP = os.path.join(tempfile.gettempdir(), "fauxstream.json")
     >>> rows = test() # generates deeply nested dictionaries
-    >>> with JSONList(TMP) as fh: # "with" tests __enter__ and __exit__
+    >>> with JSONList(TMP) as filehandle: # "with" tests __enter__ and __exit__
     ...     while True:
     ...         try:
-    ...             fh.write(flattery.flatten(next(rows)))
+    ...             filehandle.write(flattery.flatten(next(rows)))
     ...         except StopIteration:
     ...             break
     >>> # test converts to tuples so order doesn't surprise us
-    >>> with open(TMP, "r") as fh:
-    ...     for d in json.loads(fh.read()):
+    >>> with open(TMP, "r") as filehandle:
+    ...     for d in json.loads(filehandle.read()):
     ...         print([(k, d[k]) for k in sorted(d.keys())])
     [('_id', 0), ('foo', [0, 1]), ('quux', 0)]
     [('_id', 1), ('foo', [1, 2]), ('quux', 1)]
     [('_id', 2), ('foo', [2, 3]), ('quux', 2)]
-    >>> # won't totally clean up darwin, since it creates randomized temp dirs. 
+    >>> # won't totally clean up darwin, since it creates randomized temp dirs.
     >>> os.remove(TMP)
     """
     # generate deeply nested dictionaries for test.
     for i in range(3):
         yield {
             "_id": i,
-            "numbers": {
-                "foo": [i, i+1],
-                "bar": {
-                    "bat": {
-                        "baz": {
-                            "quux": i
-                        }
-                    }
-                }
-            }
+            "numbers": {"foo": [i, i + 1], "bar": {"bat": {"baz": {"quux": i}}}},
         }
+
 
 if __name__ == """__main__""":
     import doctest
+
     doctest.testmod()
